e2176894a4ef63c657720b45fa0129905f4aff6b
Jihua Yang <122041365+JayYang133@users.noreply.github.com>
2025-08-05 00:29:41 +0800

    Update VAE_train.py

--- mae/VAE/VAE_train.py
+++ mae/VAE/VAE_train.py
@@ -7,8 +7,6 @@ import torch
 from VAE_model import VAE
 import sys
 sys.path.append("..")
-#from guided_diffusion.cell_datasets import load_data
-#from guided_diffusion.cell_datasets_muris import load_data
 from guided_diffusion.cell_datasets_pbmc import load_data
 #from guided_diffusion.cell_datasets_loader import load_data
 
